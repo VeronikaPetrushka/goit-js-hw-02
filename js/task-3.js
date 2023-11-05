8c166d8f663bc754c2d4670f76de17e4e556af67
Veronika <veronikapetrushka@gmail.com>
2023-11-05 18:23:40 +0100

    done

---
+++ js/task-3.js
@@ -0,0 +1,17 @@
+function checkForSpam(message) {
+  messageLower = message.toLowerCase;
+  if (messageLower.includes(spam) || messageLower.includes(sale)) {
+    check = true;
+  } else {
+    check = false;
+  }
+  return check;
+}
+
+console.log(checkForSpam("Latest technology news")); // false
+console.log(checkForSpam("JavaScript weekly newsletter")); // false
+console.log(checkForSpam("Get best sale offers now!")); // true
+console.log(checkForSpam("Amazing SalE, only tonight!")); // true
+console.log(checkForSpam("Trust me, this is not a spam message")); // true
+console.log(checkForSpam("Get rid of sPaM emails. Our book in on sale!")); // true
+console.log(checkForSpam("[SPAM] How to earn fast money?")); // true
